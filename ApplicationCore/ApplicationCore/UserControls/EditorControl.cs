f904b2df413c95501e22663283cbc226080bbc46
pogeebo <pogeebo@gmail.com>
2012-04-18 02:11:15 +0000

    missing files...

---
+++ ApplicationCore/ApplicationCore/UserControls/EditorControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApplicationCore.UserControls
+{
+    public class EditorControl : IEditorControl
+    {
+        public event OnObjectSelected ObjectSelected;
+        protected void OnObjectSelected(object o)
+        {
+            var e = ObjectSelected;
+            if(e!=null)
+            {
+                e(this,new ObjectSelectedEventArgs(o));
+            }
+        }
+
+        private ViewModels.Editors.IEditorViewModel _viewModel;
+        public ViewModels.Editors.IEditorViewModel ViewModel
+        {
+            get
+            {
+                return _viewModel;
+            }
+            set
+            {
+                _viewModel = value;
+            }
+        }
+    }
+}
