313ed9cfd85bdc9f657bb91f971ef41b2d23d4c1
pogeebo@gmail.com <pogeebo@gmail.com@060a8afe-7c8b-6096-0acf-2b4de26f62d4>
2012-04-18 02:11:15 +0000

    missing files...

---
+++ trunk/ApplicationCore/ApplicationCore/UserControls/ITabEditorControl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApplicationCore.UserControls
+{
+    public interface ITabEditorControl : IEditorControl
+    {
+        string TabTitle { get; }
+    }
+}
