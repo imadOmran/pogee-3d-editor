5c637eb448b01cb9d66a692d90ccb088d5b75f52
pogeebo@gmail.com <pogeebo@gmail.com@060a8afe-7c8b-6096-0acf-2b4de26f62d4>
2012-04-21 04:10:47 +0000

    missing source files

---
+++ trunk/EQEmu/Files/WLD/Fragments/ModelReference.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace EQEmu.Files.WLD.Fragments
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    [Serializable]
+    public unsafe struct Fragment14Struct
+    {
+        public int flags;
+        public int fragment1;
+        public int size1;
+        public int size2;
+        public int fragment2;
+        public int params1;
+        public fixed int params2[7];        
+    };
+
+    class ModelReference
+    {
+    }
+}
---
+++ trunk/EQEmu/Files/WLD/Fragments/ObjectLocation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace EQEmu.Files.WLD.Fragments
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    [Serializable]
+    public unsafe struct Fragment15Struct
+    {
+        public int fragmentReference;
+        public int flags;             
+        public int fragment1;         
+        public float x;
+        public float y;
+        public float z;
+        public float rotateZ;
+        public float rotateY;
+        public float rotateX;
+        public fixed float params1[3];
+        public float scaleY;
+        public float scaleX;
+        public int fragment2;
+        public int params2;
+
+    };
+
+    public class ObjectLocation : Fragment<Fragment15Struct>
+    {
+        public ObjectLocation(int num)
+            : base(num)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " ObjectLocation";
+        }
+
+        public override void Handler(System.IO.Stream stream)
+        {
+            base.Handler(stream);
+        }
+
+        public float X
+        {
+            get { return this.FragmentStruct.x; }
+        }
+
+        public float Y
+        {
+            get { return this.FragmentStruct.y; }
+        }
+
+        public float Z
+        {
+            get { return this.FragmentStruct.z; }
+        }
+    }
+}
