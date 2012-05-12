00e84ae80624467ad95b66b818ff8cea2a1978a7
pogeebo <pogeebo@gmail.com>
2012-05-12 05:16:51 +0000

    missing files

---
+++ EQEmu/Spawns/NpcAggregatorLocal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using EQEmu.Database;
+
+namespace EQEmu.Spawns
+{
+    public class NpcAggregatorLocal : NpcAggregator
+    {
+        public NpcAggregatorLocal(QueryConfig config)
+            : base(config)
+        {
+
+        }
+
+        public override void Lookup(string name)
+        {
+            NPCs.Clear();
+            foreach (var npc in CachedNpcs)
+            {
+                if (npc.Name.Contains(name))
+                {
+                    AddNPC(npc);
+                }
+            }
+        }
+
+        public override int GetNextIdForZone(string zone)
+        {
+            int id = 1;
+            if (NPCs.Count > 0)
+            {
+                id += NPCs.Max(x => x.Id);
+            }
+            return id;
+        }
+
+        public override void LookupByZone(string zone)
+        {
+            foreach (var npc in CachedNpcs)
+            {
+                AddNPC(npc);
+            }
+        }
+    }
+}
