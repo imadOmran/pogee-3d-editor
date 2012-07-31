b0ded465ff3e6c5387d56d4b1be155cf201cd9b2
pogeebo@gmail.com <pogeebo@gmail.com@060a8afe-7c8b-6096-0acf-2b4de26f62d4>
2012-07-31 00:56:10 +0000

    added the "database plugin" - to remove the main application dependency

--- trunk/ApplicationCore/ApplicationCore/MainWindowViewModel.cs
+++ trunk/ApplicationCore/ApplicationCore/MainWindowViewModel.cs
@@ -105,40 +105,6 @@ namespace ApplicationCore
                 return;
             }
 
-            #region register database connection configuration
-            EQEmu.Database.Configuration conf = new EQEmu.Database.Configuration();
-            MySqlConnection connection = GetDatabaseConnection(ref conf);
-            if (connection != null)
-            {
-                _container.RegisterInstance<MySqlConnection>(connection);
-            }
-            else
-            {
-                _container.RegisterInstance<MySqlConnection>(new MySqlConnection());
-                System.Windows.MessageBox.Show("Could not create database connection, configure connection.xml and restart");
-            }
-
-            _container.RegisterInstance<EQEmu.Database.Configuration>(conf);
-            #endregion
-
-            #region register the type to query mappings
-
-            XDocument doc;
-            EQEmu.Database.QueryConfig config = null;
-
-            if (!DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
-            {
-#if EQEMU
-                doc = XDocument.Load("eqemu-config.xml");
-#else
-                doc = XDocument.Load("config.xml");
-#endif
-                config = EQEmu.Database.QueryConfig.Create(doc.Root);
-                _container.RegisterInstance<EQEmu.Database.QueryConfig>(config);
-            }            
-
-            #endregion
-
             #region register 3D 'things' transforms / cameras / etc
 
             _container.RegisterInstance<ProjectionCamera>(new PerspectiveCamera()
@@ -364,54 +330,6 @@ namespace ApplicationCore
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(info));
             }
-        }
-
-        private MySqlConnection GetDatabaseConnection(ref EQEmu.Database.Configuration configuration)
-        {
-            XmlSerializer serializer =
-                new XmlSerializer(typeof(EQEmu.Database.Configuration));
-            MySql.Data.MySqlClient.MySqlConnection conn = null;
-
-            var confPath = System.IO.Path.Combine(
-                System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData),
-                "EQEmu/connection.xml");
-
-            //check for a configuration file in the user directory first
-            if (!File.Exists(confPath))
-            {
-                confPath = "./connection.xml";
-                if (!File.Exists(confPath))
-                {
-                    return null;
-                }
-            }
-
-            EQEmu.Database.Configuration conf;
-
-            using (XmlReader reader = XmlReader.Create(confPath))
-            {
-                conf = (EQEmu.Database.Configuration)serializer.Deserialize(reader);
-                string connStr = string.Format("server={0};user={1};database={2};port={3};password={4};ConnectionTimeout={5};",
-                    conf.Host, conf.User, conf.Database, conf.Port, conf.Password, conf.ConnectionTimeout);
-                conn = new MySql.Data.MySqlClient.MySqlConnection(connStr);
-                try
-                {
-                    conn.Open();
-                }
-                catch (System.Exception e)
-                {
-                    conn.Close();
-
-                    System.Windows.MessageBox.Show(confPath+"\nCould not open database connection\n"+ e.Message);
-                }
-            }
-
-            if (configuration != null && conf != null)
-            {
-                configuration = conf;
-            }
-
-            return conn;
         }
     }
 }
